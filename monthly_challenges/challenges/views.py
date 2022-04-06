76da0fd0184664f0ae271051c846c5bf6df2d7cc
Cameron <swishercameron0@gmail.com>
2022-04-05 22:14:20 -0500

    add html template, render template functin in views, add monthly_challenges to apps in settings.py

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,6 +2,7 @@ from urllib import response
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 # Create your views here.
 
 monthly_challenges = {
@@ -43,8 +44,10 @@ def monthly_challenge_by_number(request, month):
 
 def monthly_challenge(request, month):
     try:
-        challenge_text = f"<h1>{monthly_challenges[month]}<h1>"
-        return HttpResponse(challenge_text)
+        challenge_text = monthly_challenges[month]
+        return render(request, "challenges/challenge.html")
+        # response_data = render_to_string("challenges/challenge.html")
+        # return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("please select an address with a correct coresponding month")
 
